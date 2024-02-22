45301b373dca78d3993d48aadbd438e064a79d77
BenItoIBa <159397548+BenItoIBa@users.noreply.github.com>
2024-02-23 12:55:53 +1300

    Add files via upload

--- sketch_feb22a.ino
+++ sketch_feb22a.ino
@@ -1,38 +1,70 @@
-#include <Servo.h>
+#include <Adafruit_LiquidCrystal.h>
 
-const byte LEDPIN = 13;
 const byte BINPIN = 1;
-const byte MICROPIN = 2;
-const byte SERVOPIN = 3;
+const byte NOISEPIN = A0;
+const byte POTPIN = A1;
 
-const byte LOCK = 0;
-const byte UNLOCK = 90;
-const byte LOUDENOUGH = 512;
-
-
-Servo latch;
-
+Adafruit_LiquidCrystal screen(0);
 
 void setup() {
-  pinMode(LEDPIN, OUTPUT);
-  pinMode(BINPIN, INPUT);
-  pinMode(MICROPIN, INPUT);
-  latch.attach(SERVOPIN);
+  pinmode(BINPIN,INPUT);
+pinmode(POTPIN,INPUT);
+}
 
-  Serial.begin(9600);
+randomSeed(analogRead(NOISEPIN));
+
+Serial.begin(9600);
+
+
+if (!screen.begin(16,2)) {
+  Serial.println("couldn't start the screen? check wiring");
+  while(1):
 }
 
 void loop() {
-
-  if (analogRead(MICROPIN) > LOUDENOUGH) {
-    Serial.print("Heard a knock, unlocking box");
-    digitalWrite(LEDPIN, HIGH);
-    latch.write(UNLOCK);
+  if(digitalRead(BINPIN) == HIGH) {
+    Serial.println("Rolling Dice");
+    int diceType = analogRead(POTPIN);
+    diceType = map(diceType, 0, 1023, 1, 6);
+    rollDice(diceType);
   }
+}
+void rollDice(byte numSides) {
+  byte  diceRoll = 255;
 
+  switch (numSides) {
+    case 1:
+    screen.print("Roll is: ");
+    diceRoll= random(1, 4);
+    break;
 
-  if (digitalRead(BINPIN) == HIGH) {
-    Serial.print("Button pressed, locking box");
-    latch.write(LOCK);
+    case 2:
+    screen.print("Roll is: ");
+    diceRoll= random(1, 6);
+    break;
+
+    case 3:
+    screen.print("Roll is: ");
+    diceRoll= random(1, 8);
+    break;
+
+    case 4:
+    screen.print("Roll is: ");
+    diceRoll= random(1, 10);
+    break;
+
+    case 5:
+    screen.print("Roll is: ");
+    diceRoll= random(1, 12);
+    break;
+
+    case 6:
+    screen.print("Roll is: ");
+    diceRoll= random(1, 20);
+    break;
+    default:
+    Serial.println("Issue with mapping potentiometer values");
+    break;
   }
+  screen.println(diceRoll)
 }
